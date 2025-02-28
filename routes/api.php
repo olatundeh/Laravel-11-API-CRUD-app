f7dfea8c80189b099e658ac7fa99f1b3bf12a73d
Olatunde Haroun <haroun.ola@gmail.com>
2025-02-28 22:43:20 +0000

    Updated api route file

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get('/', function () {
-    return 'API is working';
-});
+Route::apiResource('posts', PostController::class);
